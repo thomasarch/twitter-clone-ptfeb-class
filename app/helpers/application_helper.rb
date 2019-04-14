6f2f7bfbba56c5ff30d3f85f8dd5f4a91a84ee92
Thomas DeFelice <thomasarch@gmail.com>
2019-04-14 15:35:24 -0400

    displays profile pics in navbar and tweet card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     message = Faker::TvShows::RickAndMorty.quote
     Tweet.create!(message: message, user_id: user_id)
   end
+
+  def profile_pic(user)
+    if user.autopic != nil
+      user.autopic
+    elsif user.avatar.url != nil
+      user.avatar.url
+    else
+      'default.png'
+    end
+  end
 end
