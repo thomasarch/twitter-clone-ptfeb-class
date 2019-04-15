947c8a5978f87e8faa8fcb20670871410006d319
Thomas DeFelice <thomasarch@gmail.com>
2019-04-15 16:47:09 -0400

    changed back to twin peaks faker

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def random_tweet
     user_id = User.where(bot: true).sample.id
-    message = Faker::TvShows::RickAndMorty.quote
+    message = Faker::TvShows::TwinPeaks.quote
     Tweet.create!(message: message, user_id: user_id)
   end
 
